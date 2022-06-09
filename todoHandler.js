505bcc368fab1b0cf1161c97d576a2761f964fb0
Rezzak <87976069+rezzakali@users.noreply.github.com>
2022-06-09 22:33:07 +0530

    first commit

--- todoHandler.js
+++ todoHandler.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 const express = require('express');
-const Todo = require('../schemas/todoSchema');
+const Todo = require('./todoSchema');
 
 const router = express.Router();
 
